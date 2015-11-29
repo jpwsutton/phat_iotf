2bc015e07b717ddf21a1b1a7fb618591adad853c
James Sutton <jpwsutton@gmail.com>
2015-11-29 20:02:15 +0000

    Adding IBM IoTF Functionality

--- iotf_phat.py
+++ iotf_phat.py
@@ -1,2 +1,46 @@
+try:
+    import ibmiotf.application
+    import ibmiotf.device
+except:
+    print("Could not find the IBM IoTF library")
+    sys.exit()
+    
+shutdown_flag = False
+
+
+def myAppEventCallback(event):
+    print("Received live data from %s (%s) sent at %s: hello%s x=%s" % (event.deviceId, event.deviceType, event.timestamp.strftime("%H:%M:%S"), data['hello'], data['x']))
+
+def processCommand(data):
+  print("New Data: %s" % data)
+
+def commandCallback(cmd):
+  if cmd.command == "shutdown":
+        if 'shutdown' not in cmd.data:
+            print("Error incorrect shutdown command")
+        else:
+            if cmd.data['shutdown'] is True:
+                print("Shutdown received, goodbye!")
+                global shutdown_flag
+                shutdown_flag = True
+    elif cmd.command == "command":
+        processCommand(cmd.data)
+  
+try:
+    options = ibmiotf.device.ParseConfigFile("/home/pi/git/mqttbot/device.cfg")
+    client = ibmiotf.device.Client(options)
+    client.commandCallback = commandCallback
+    client.connect()
+except Exception as e:
+    print("Could not connect: %s" % str(e))
+    sys.exit()
+    
+while True:
+    if shutdown_flag is True:
+        print("Shutdown Flag detected, goodbye")
+        client.disconnect()
+        sys.exit(0)
+    else:
+        time.sleep(2)
 print("IoTF pHat Controller")
 
